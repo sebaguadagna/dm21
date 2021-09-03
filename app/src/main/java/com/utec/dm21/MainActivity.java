c757e80ba199f33d33c955abd7d81e8616f77cb0
Sebastian Oscar Guadagna Scavone <sebastian.guadagna@globant.com>
2021-09-03 14:12:40 -0300

    all files about android Project

---
+++ app/src/main/java/com/utec/dm21/MainActivity.java
@@ -0,0 +1,14 @@
+package com.utec.dm21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
